6b2963a26670ceb8e437408c63344aaf4a4afb16
James Honeyball <jhoneyball@Mac-mini-i7.local>
2017-07-09 16:05:23 +0100

    Initial Commit

---
+++ VideoBrowserDataSources/VideoBrowserDataSources.h
@@ -0,0 +1,19 @@
+//
+//  VideoBrowserDataSources.h
+//  VideoBrowserDataSources
+//
+//  Created by James Honeyball on 09/07/2017.
+//  Copyright © 2017 James Honeyball. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for VideoBrowserDataSources.
+FOUNDATION_EXPORT double VideoBrowserDataSourcesVersionNumber;
+
+//! Project version string for VideoBrowserDataSources.
+FOUNDATION_EXPORT const unsigned char VideoBrowserDataSourcesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VideoBrowserDataSources/PublicHeader.h>
+
+
